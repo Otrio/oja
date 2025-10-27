b98535bb5ba3dd3b9e422df6ae089f4d8e7fe632
Otrio <oyewoleolusoji13@gmail.com>
2025-10-27 03:49:41 -0700

    setup core web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import './styles/supabase-theme.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <SpeedInsights />
     </AuthProvider>
   </StrictMode>,
 )
